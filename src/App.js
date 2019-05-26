54acf10ca612b7e6330ca2e3c8d6d3979ae0c91d
sanra9 <sanrra999@gmail.com>
2019-05-26 17:40:27 -0500

    jé

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      buu: false,
+    }
+    this.esconde = this.esconde.bind(this)
+  }
+
   render() {
+    const content  = this.state.buu ? <div> <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> </div> : null;
     return (
       <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <label><input type="checkbox" onClick={this.esconde} /> Mostrar información importante</label>
+        { content }
       </div>
+
     );
   }
+
+  esconde(){
+    this.setState(state => ({
+      buu: !state.buu
+    }))
+  }
 }
+
 
 export default App;
